e07a3ccfdf794b52e34d61c214f827b94d3e0735
poan_yang <poan_yang@trendmicro.com>
2016-06-11 14:46:51 +0800

    Modify the setup tool and __main__

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="pytext2speech",
-    packages=["pytext2speech"],
+    packages=find_packages(),
     version="0.0.1",
     description="",
     author="PyText2Speech",
@@ -14,6 +14,9 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
     ],
+    entry_points={'console_scripts': [
+        'text2speech = text_to_speech.__main__:main',
+    ]},
     long_description="""\
 Text to Speech package
 -------------------------------------
--- text_to_speech/__main__.py
+++ text_to_speech/__main__.py
@@ -22,8 +22,7 @@ DESCRIPTION
 	-v, --version
 
 EXAMPLES
-    text_to_speech -n USERNAME -p PASSWORD TEXT TO SPPECH
-
+    text2speech -o output.mp3 -s google  Have a goode day
 
 COPYRIGHT
 	MIT Licence
@@ -35,40 +34,49 @@ SOURCE
 import sys
 import getopt
 from gtts import gTTS
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "o:s:n:p:hv", ["output", "service", "name", "password", "help", "version"])
-except getopt.GetoptError as e:
-    print(__doc__)
-    sys.exit("invalid option")
 
-name = None
-password = None
-service_type = 'google'
-output_file = 'out.mp3'
 
-for o, a in opts:
-    if o in ('-h', '--help'):
+def main(argv=None):
+    if not argv:
+        argv = sys.argv[1:]
+
+    try:
+        opts, args = getopt.getopt(argv, "o:s:n:p:hv", ["output", "service", "name", "password", "help", "verbose"])
+    except getopt.GetoptError as e:
         print(__doc__)
-        sys.exit(0)
-    elif o in ('-v', '--version'):
-        print(0.1)
-        sys.exit(0)
-    elif o in ('-n', '--name'):
-        name = a
-    elif o in ('-p', '--password'):
-        password = a
-    elif o in ('-s', '--service'):
-        service_type = a
-    elif o in ('-o', '--output'):
-        output_file = a
+        sys.exit("invalid option: " + str(e))
 
-text_need_to_speech = ' '.join(args)
+    name = None
+    password = None
+    service_type = 'google'
+    output_file = 'out.mp3'
+    verbose = False
 
-if service_type == 'google':
-    tts = gTTS(text='Hello 123', lang='en')
-    tts.save(output_file)
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            print(__doc__)
+            sys.exit(0)
+        elif o in ('-v', '--verbose'):
+            verbose = True
+        elif o in ('-n', '--name'):
+            name = a
+        elif o in ('-p', '--password'):
+            password = a
+        elif o in ('-s', '--service'):
+            service_type = a
+        elif o in ('-o', '--output'):
+            output_file = a
 
-else:
-    sys.exit("invalid service type")
+    text_need_to_speech = ' '.join(args)
+
+    if verbose:
+        print('[{}] {} > {}'.format(service_type, text_need_to_speech, output_file))
+    if service_type == 'google':
+        tts = gTTS(text=text_need_to_speech, lang='en')
+        tts.save(output_file)
+    else:
+        sys.exit("invalid service type")
 
 
+if __name__ == "__main__":
+    main(sys.argv[1:])
