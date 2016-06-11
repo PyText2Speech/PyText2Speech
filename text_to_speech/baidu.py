4fcba22abd646b6bbc655ded22fd4edf21a4b628
tim <tim.yellow@gmail.com>
2016-06-11 15:05:22 +0800

    baidu check input more strict

--- text_to_speech/baidu.py
+++ text_to_speech/baidu.py
@@ -10,12 +10,15 @@ class BaiduSpeech(Speech):
     def __init__(self, name, password):
         super(BaiduSpeech, self).__init__(name, password)
 
-        uri = "https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=%s&client_secret=%s" % (
-            name, password)
+        uri = "https://openapi.baidu.com/oauth/2.0/token"
 
-        resp = requests.get(uri).json()
+        params = {
+            'grant_type': 'client_credentials',
+            'client_id': name,
+            'client_secret': password,
+        }
 
-        print(resp)
+        resp = requests.get(uri, params=params).json()
 
         if 'error' in resp:
             raise AuthenticationError(resp['error_description'])
@@ -27,6 +30,9 @@ class BaiduSpeech(Speech):
         spd = kwargs.get('spd', 5)
         pit = kwargs.get('pit', 5)
         vol = kwargs.get('vol', 5)
+
+        if 'zh' in lang:
+            lang = 'zh'
 
         response = requests.get("http://tsn.baidu.com/text2audio", {
             "tex": narration.encode('utf-8'),
@@ -57,7 +63,7 @@ class BaiduSpeech(Speech):
 if __name__ == "__main__":
 
     baidu = BaiduSpeech("hkOIhq0imbfhzxGsxq2HwYN7", "27c99621b1c7b2777ce054442c15382b")
-    content = baidu.speech(u"很高興參加這個project", u'zh-cn')
+    content, extension = baidu.speech(u"很高興參加這個project", u'zh-cn')
 
-    with open("/tmp/test.mp3", 'wb') as fp:
+    with open("/tmp/test." + extension, 'wb') as fp:
         fp.write(content)
