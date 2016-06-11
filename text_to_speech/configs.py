b239f70860c63cdfffafb8bd92dc60ab429984bd
chairco <chairco@gmail.com>
2016-06-11 19:49:55 +0800

    modify configs.py to read '~/pytext2speech.json' setting account info

--- text_to_speech/configs.py
+++ text_to_speech/configs.py
@@ -1,23 +1,35 @@
 # -*- encoding=utf8 -*-
+import json
+import os
 
-name = 'name'
-pwd = 'pwd'
+from pprint import pprint
 
-server = {
-            "BAIDU" : {
-                        name: 'hkOIhq0imbfhzxGsxq2HwYN7',
-                        pwd: '27c99621b1c7b2777ce054442c15382b'
-            },
-            "GOOGLE" : {
-                        name: '',
-                        pwd: ''
-            },
-            "WATSON" : {
-                        name: 'adb36e85-0df9-446c-b168-741eb5439c50',
-                        pwd: 'jxgksFkuZ74o'
-            },
-            "ITIR" : {
-                        name: '',
-                        pwd: ''
+file = os.path.expanduser('~/pytext2speech.json')
+ck_key = ['BAIDU', 'ITRI', 'WATSON']
+
+try:
+    with open(file, 'r') as fp:
+        server = json.load(fp)
+        for k, v in server.items():
+            if k in ck_key: 
+                if v['name'] == '' or v['pwd'] == '':
+                    raise ValueError(str(k), u'JSON setting is empty,' \
+                                    'name = ',str(v['name']), 'pwd = ',\
+                                    str(v['pwd']))
+    fp.close()
+except Exception as e:
+    server = {
+                'BAIDU': {'name': '','pwd': ''},
+                'GOOGLE': {'name': '', 'pwd': ''},
+                'ITRI': {'name': '', 'pwd': ''},
+                'WATSON': {'name': '','pwd': ''}
             }
-}
+    with open(file, 'w') as fp:
+        json.dump(server, fp)
+    fp.close()
+    msg = "Not exist pytext2speech.json file at %s. " \
+          "Already create JSON file, please maintenance it." \
+          "message." % os.path.expanduser('~/')
+    raise Exception(msg)
+except ValueError as e:
+    raise Exception(e)
