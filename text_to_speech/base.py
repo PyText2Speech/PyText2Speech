8cda9c1f75e046c8afdfdedf6511b94cf3a612fb
tim <tim.yellow@gmail.com>
2016-06-11 11:11:08 +0800

    base change

--- text_to_speech/base.py
+++ text_to_speech/base.py
@@ -2,7 +2,7 @@ class Speech(object):
     def __init__(self, name, password):
         pass
 
-    def speech(self, narration, lang):
+    def speech(self, narration, lang, **kwargs):
         pass
 
     def meta(self):
