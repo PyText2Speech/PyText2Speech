97d595dbecf495bdca55a05450d7d5d66ff5f3de
poan_yang <poan_yang@trendmicro.com>
2016-06-11 13:36:23 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+    name="pytext2speech",
+    packages=["pytext2speech"],
+    version="0.0.1",
+    description="",
+    author="PyText2Speech",
+    author_email="",
+    url="https://github.com/PyText2Speech",
+    download_url="",
+    # keywords=["encoding", "i18n", "xml"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    long_description="""\
+Text to Speech package
+-------------------------------------
+
+Speech text integrate with google, IBM watson, Industrial Technology Research Institute of Taiwan, Baidu.
+
+This version requires Python 3.
+"""
+)
