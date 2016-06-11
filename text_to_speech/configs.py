5a93ff585715aa377cc18de302a968468f0b82b8
chairco <chairco@gmail.com>
2016-06-11 19:53:36 +0800

    add more raise error msg

--- text_to_speech/configs.py
+++ text_to_speech/configs.py
@@ -6,7 +6,6 @@ from pprint import pprint
 
 file = os.path.expanduser('~/pytext2speech.json')
 ck_key = ['BAIDU', 'ITRI', 'WATSON']
-
 try:
     with open(file, 'r') as fp:
         server = json.load(fp)
@@ -16,7 +15,6 @@ try:
                     raise ValueError(str(k), u'JSON setting is empty,' \
                                     'name = ',str(v['name']), 'pwd = ',\
                                     str(v['pwd']))
-    fp.close()
 except Exception as e:
     server = {
                 'BAIDU': {'name': '','pwd': ''},
@@ -26,10 +24,9 @@ except Exception as e:
             }
     with open(file, 'w') as fp:
         json.dump(server, fp)
-    fp.close()
     msg = "Not exist pytext2speech.json file at %s. " \
           "Already create JSON file, please maintenance it." \
           "message." % os.path.expanduser('~/')
-    raise Exception(msg)
+    raise Exception(msg, e)
 except ValueError as e:
     raise Exception(e)
