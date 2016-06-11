7b64633c312dbb1e535fab24347d72a8a8a5eef4
tim <tim.yellow@gmail.com>
2016-06-11 11:38:13 +0800

    update baidu.txt

--- text_to_speech/baidu.py
+++ text_to_speech/baidu.py
@@ -1,2 +1,51 @@
-from .base import  Speech
+# -*- encoding=utf8 -*-
+import requests
+import os
 
+from .base import Speech
+
+
+class BaiduSpeech(Speech):
+    def __init__(self, name, password):
+        super(BaiduSpeech, self).__init__(name, password)
+
+        uri = "https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=%s&client_secret=%s" % (
+            name, password)
+
+        resp = requests.get(uri).json()
+
+        print(resp)
+
+        self.token = resp['access_token']
+
+    def speech(self, narration, lang, **kwargs):
+
+        spd = kwargs.get('spd', 5)
+        pit = kwargs.get('pit', 5)
+        vol = kwargs.get('vol', 5)
+
+        print(self.token)
+
+        response = requests.get("http://tsn.baidu.com/text2audio", {
+            "tex": narration.encode('utf-8'),
+            "lan": lang,
+            "tok": self.token,
+            "ctp": 1,
+            "spd": spd,
+            "pit": pit,
+            "vol": vol,
+            "cuid": "xxx-xxx-xxx-xxx"
+        })
+
+        print(response)
+
+        return response.content
+
+
+if __name__ == "__main__":
+
+    baidu = BaiduSpeech("hkOIhq0imbfhzxGsxq2HwYN7", "27c99621b1c7b2777ce054442c15382b")
+    content = baidu.speech(u"很高興參加這個project", u'zh')
+
+    with open("/tmp/test.mp3", 'wb') as fp:
+        fp.write(content)
